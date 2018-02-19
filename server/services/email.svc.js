9f83fe0b5a799b5f2ca6853ed4a69d966c5cd8ab
Luke Robinson <lukerobinson@Lukes-MBP-2.attlocal.net>
2018-02-19 11:48:09 -0500

    commit

---
+++ server/services/email.svc.js
@@ -0,0 +1,14 @@
+var mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.DOMAIN_KEY });
+
+var from_who = 'covalenceportal@gmail.com';
+
+exports.sendEmail = function (toAddress) {
+    var data = {
+        to: toAddress,     
+        from: from_who,
+        subject: 'Covalence store purchase',
+        html: 'Thank you for your recent purchase. A breakdown of your purchase can be seen here, and please let us know of any questions or concerns!'
+      };
+       
+      return mailgun.messages().send(data);
+}
